c9f495a711f813c27d3e45288852035386cfd620
Rausell <arlopez6@dons.usfca.edu>
2023-11-19 19:16:23 -0800

    Creating default parameters

--- elevatorProgram.java
+++ elevatorProgram.java
@@ -1,7 +1,21 @@
 //Angel Lopez Project02 
+//Importing needed modules
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.*;
 
 public class elevatorProgram{
 	public static void main(String[] args) {
 		System.out.println("Testing program runs successfully.");
+
+		//Establishing default values
+		int floors = 32;
+		double passengers = 0.03;
+		int elevators = 1;
+		int elevatorCap = 10;
+		int ticks = 500;
+
+		
 	}
 }

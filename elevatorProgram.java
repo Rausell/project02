5b29a4a224853ea13c6762f13fff59cec2543439
Rausell <arlopez6@dons.usfca.edu>
2023-11-19 19:23:32 -0800

    reading/close test
    Testing reading / closing file & attaching given values from read file

--- elevatorProgram.java
+++ elevatorProgram.java
@@ -16,6 +16,28 @@ public class elevatorProgram{
 		int elevatorCap = 10;
 		int ticks = 500;
 
-		
+		/*
+		Following block checks for existence of a given file
+		& reads/updates values as listed in file.
+		It also deals with any error that might arise from the file
+		*/
+
+		if(args.length > 0){
+			try{
+				FileInputStream file = new FileInputStream(args[0]);
+				Properties properties = new Properties();
+				properties.load(file);
+
+				//Assigning given 'Key' names to appropriate variable
+				//Default values still at play if no matching 'Key' exists				
+				floors = Integer.parseInt(properties.getProperty("floors", "32"));
+				passengers = Double.parseDouble(properties.getProperty("passengers", "0.03"));
+
+				file.close();
+			} catch (IOException e){
+
+			}
+		}
+
 	}
 }
